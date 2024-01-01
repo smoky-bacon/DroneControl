5da5eed9536c90c700473e3c7e2d75f6bc0735c9
smoky-bacon <155300791+smoky-bacon@users.noreply.github.com>
2024-01-02 00:12:42 +0900

    Update Motor.py

--- Motor.py
+++ Motor.py
@@ -1,1 +1,15 @@
 import math
+
+class PMMotor:
+
+  def __init__(self,Kt,Ke,phi,Ra):
+    self.Kt = Kt
+    self.Ke = Ke
+    self.phi = phi
+    self.Ra = Ra
+
+  def GetTorque(self,omega,Va):
+    k1 = self.Kt*self.Ke*self.phi*self.phi/self.Ra
+    k2 = self.Kt*self.phi/self.Ra
+    return - k1*omega + k2*Va
+
